993271ce3a85145cd9937604dd0f9c8b1c6eb69f
Diego Almeida <dyhalmeida@gmail.com>
2022-04-24 19:02:43 -0300

    refactor: add correctly interface to SignUpController.handle method

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,6 +1,8 @@
+import { HttpRequest, HttpResponse } from '../protocols/http'
+
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 export class SignUpController {
-  public handle (httpRequest: any): any {
+  public handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
