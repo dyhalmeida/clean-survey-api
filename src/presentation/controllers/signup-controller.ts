e7c7f9b967254c923701f91a9bcbd411bb245cbb
Diego Almeida <dyhalmeida@gmail.com>
2022-04-24 18:27:01 -0300

    feat: ensure that SignUpController returns 400 when e-mail is not provided

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,8 +1,18 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 export class SignUpController {
   public handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      }
     }
   }
 }
